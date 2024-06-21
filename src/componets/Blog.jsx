22725e9b9cc38febc9746baf0279a811080ba3f4
Alex Karobia <akarobia30@gmail.com>
2024-06-21 11:07:31 +0300

    Font rectification and skills card height fixing

--- src/componets/Blog.jsx
+++ src/componets/Blog.jsx
@@ -43,7 +43,7 @@ const Blog = () => {
                 {Skill.map((data, index) => (
                   <div
                     key={index}
-                    className="transform transition duration-500 hover:scale-105 relative text-white  bg-gradient-to-tl from-white to-secondary rounded-xl shadow-md p-4 flex flex-col items-center justify-center "
+                    className="transform transition duration-500 hover:scale-105 relative text-white bg-gradient-to-b from-white to-secondary rounded-xl shadow-md p-4 flex flex-col items-center justify-center "
                     onMouseEnter={() => handleSkillHover(data.id)}
                     onMouseLeave={handleSkillLeave}
                     style={{ maxWidth: '300px', minWidth: '250px' }} // Adjusting maximum and minimum width
